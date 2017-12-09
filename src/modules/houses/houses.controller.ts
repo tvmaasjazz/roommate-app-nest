bdcb4968fe34411a2267268f3e9a4b4533ea0eab
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-12-09 17:19:47 -0500

    Add houses controller

---
+++ src/modules/houses/houses.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateHouseDto } from './dto/create-house.dto';
+import { HousesService } from './houses.service';
+import { House } from './house.entity';
+
+@Controller('houses')
+export class HousesController {
+  constructor(private readonly housesService: HousesService) {}
+
+  @Post()
+  async create(@Body() createHouseDto: CreateHouseDto) {
+    await this.housesService.create(createHouseDto);
+  }
+
+  @Get()
+  async findAll(): Promise<House[]> {
+    return await this.housesService.findAll();
+  }
+}
