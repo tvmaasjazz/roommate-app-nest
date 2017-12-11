00ec19b291bbf71216acd3e641deaa751f091509
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-12-11 18:05:06 -0500

    Refactor users.controller to use Observables instead of Promises

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,29 +1,31 @@
 import { Controller, Get, Post, Put, Body, Param } from '@nestjs/common';
+import { Observable } from 'rxjs/Observable';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { House } from '../houses/house.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get(':username')
-  async findUserByUsername(@Param() params) {
-    return await this.usersService.findUserByUsername(params.username);
+  findUserByUsername(@Param() params): Observable<User> {
+    return this.usersService.findUserByUsername(params.username);
   }
 
   @Post('addUser')
-  async create(@Body() createUserDto: CreateUserDto) {
-    return await this.usersService.create(createUserDto);
+  create(@Body() createUserDto: CreateUserDto): Observable<User> {
+    return this.usersService.create(createUserDto);
   }
 
   @Put(':id/updateProfile')
-  async editUser(@Body() createUserDto: CreateUserDto, @Param() params) {
-    return await this.usersService.editUser(createUserDto, params.id);
+  editUser(@Body() createUserDto: CreateUserDto, @Param() params) {
+    return this.usersService.editUser(createUserDto, params.id);
   }
 
   @Put(':id/joinHouse')
-  async joinHouse(@Body() body, @Param() params) {
-    return await this.usersService.joinHouse(body.key, params.id);
+  joinHouse(@Body() body, @Param() params): Observable<House> {
+    return this.usersService.joinHouse(body.key, params.id);
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,11 +1,12 @@
 import { Component } from '@nestjs/common';
+import { Observable } from 'rxjs/Observable';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
 import { House } from '../houses/house.entity';
 
 @Component()
 export class UsersService {
-  async create(createUserDto: CreateUserDto): Promise<User> {
+  create(createUserDto: CreateUserDto): Observable<User> {
     const user = new User();
     user.username = createUserDto.username;
     user.firstName = createUserDto.firstName;
@@ -13,21 +14,31 @@ export class UsersService {
     user.phone = createUserDto.phone;
     user.imageUrl = createUserDto.imageUrl;
 
-    return await user.save();
+    return Observable.fromPromise(user.save());
   }
 
-  async findUserByUsername(username: string): Promise<User> {
-    return await User.findOne<User>({ where: { username } });
+  findUserByUsername(username: string): Observable<User> {
+    return Observable.fromPromise(
+      User.findOne<User>({ where: { username } })
+    );
   }
 
-  async editUser(createUserDto: CreateUserDto, id: string) {
-    return await User.update<User>(createUserDto, { where: { id } });
+  editUser(createUserDto: CreateUserDto, id: string) {
+    return Observable.fromPromise(
+      User.update<User>(createUserDto, { where: { id } })
+    );
   }
 
   // would want to inject houseService and have func that finds by key?
-  async joinHouse(key: string, id: string): Promise<House> {
-    const house = await House.findOne<House>({ where: { key } });
-    await User.update({ houseId: house.id }, { where: { id } });
-    return house;
+  joinHouse(key: string, id: string): Observable<House> {
+    const houseStream = Observable.fromPromise(
+      House.findOne<House>({ where: { key } })
+    );
+
+    houseStream.subscribe((house) => {
+      User.update({ houseId: house.id }, { where: { id } });
+    });
+    
+    return houseStream;
   }
 }
