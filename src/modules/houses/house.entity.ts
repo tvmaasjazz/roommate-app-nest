2e9b0f78ab9bfd31b6f21e3f9dcfcb7f7f2b3e43
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-12-10 22:19:47 -0500

    Add additional specification to House model

--- src/modules/houses/house.entity.ts
+++ src/modules/houses/house.entity.ts
@@ -1,10 +1,19 @@
-import { Table, Column, Model } from 'sequelize-typescript';
+import { Table, Column, Model, PrimaryKey, AutoIncrement, HasMany } from 'sequelize-typescript';
+import { User } from '../users/user.entity';
 
 @Table
 export class House extends Model<House> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column
+  id: number;
+
   @Column
   name: string;
 
   @Column
   key: string;
+
+  @HasMany(() => User)
+  users;
 }
