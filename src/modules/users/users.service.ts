836c271308a5c0ff3f5ebd993584e4d7e48435da
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-12-10 23:17:53 -0500

    Fix spelling error and remove incorrect return type

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -5,7 +5,7 @@ import { House } from '../houses/house.entity';
 
 @Component()
 export class UsersService {
-  aysnc create(createUserDto: CreateUserDto): Promise<User> {
+  async create(createUserDto: CreateUserDto): Promise<User> {
     const user = new User();
     user.username = createUserDto.username;
     user.firstName = createUserDto.firstName;
@@ -20,13 +20,13 @@ export class UsersService {
     return await User.findOne<User>({ where: { username } });
   }
 
-  async editUser(createUserDto: CreateUserDto, id: string): Promise<User> {
+  async editUser(createUserDto: CreateUserDto, id: string) {
     return await User.update<User>(createUserDto, { where: { id } });
   }
 
   // would want to inject houseService and have func that finds by key?
-  async joinHouse(id: string): Promise<House> {
-    const house = await House.findOne<House>({ where: { key: id } });
+  async joinHouse(key: string, id: string): Promise<House> {
+    const house = await House.findOne<House>({ where: { key } });
     await User.update({ houseId: house.id }, { where: { id } });
     return house;
   }
