0c74d93dd5777f0d7ecfe181154c1c734dfa64cf
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-12-11 16:13:58 -0500

    Add logger and profiler middlwares to app.module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,8 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewaresConsumer } from '@nestjs/common';
 import { HousesModule } from './houses/houses.module';
 import { UsersModule } from './users/users.module';
+import { HousesController } from './houses/houses.controller';
+import { UsersController } from './users/users.controller';
+import { loggerMiddleware } from './common/middlewares/logger.middleware';
+import { ProfilerMiddleware } from './common/middlewares/profiler.middleware';
 
 @Module({
   modules: [HousesModule, UsersModule],
 })
-export class ApplicationModule {}
+export class ApplicationModule implements NestModule {
+  configure(consumer: MiddlewaresConsumer) {
+    consumer
+      .apply(ProfilerMiddleware)
+      .forRoutes(HousesController, UsersController);
+
+    consumer
+      .apply(loggerMiddleware)
+      .forRoutes(HousesController, UsersController);
+  }
+}
