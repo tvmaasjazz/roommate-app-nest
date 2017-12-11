009fd0f01c0fb51a38f0cf9f893d57aeb1a8e78c
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-12-11 10:34:28 -0500

    Write unit tests for houses.controller

---
+++ src/modules/houses/houses.controller.spec.ts
@@ -0,0 +1,32 @@
+import { Test } from '@nestjs/testing';
+import { HousesController } from './houses.controller';
+import { HousesService } from './houses.service';
+import { CreateHouseDto } from './dto/create-house.dto';
+
+describe('HousesController', () => {
+  let housesController: HousesController;
+  let housesService: HousesService;
+
+  beforeEach(() => {
+    housesService = new HousesService();
+    housesController = new HousesController(housesService);
+  });
+
+  describe('create', () => {
+    it('should return a house', async () => {
+      const result = 'a house';
+      jest.spyOn(housesService, 'create').mockImplementation(() => result);
+
+      expect(await housesController.create(new CreateHouseDto)).toBe(result);
+    });
+  });
+
+  describe('findHouseById', () => {
+    it('should return a house', async () => {
+      const result = 'a house';
+      jest.spyOn(housesService, 'findHouseById').mockImplementation(() => result);
+
+      expect(await housesController.findHouseById('1')).toBe(result);
+    });
+  });
+});
