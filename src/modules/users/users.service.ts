32864aadf2f3539227cf36faa99265c53f166fb5
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-12-10 19:57:23 -0500

    Add user service

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Component } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
+import { House } from '../houses/house.entity';
+
+@Component()
+export class UsersService {
+  aysnc create(createUserDto: CreateUserDto): Promise<User> {
+    const user = new User();
+    user.username = createUserDto.username;
+    user.firstName = createUserDto.firstName;
+    user.lastName = createUserDto.lastName;
+    user.phone = createUserDto.phone;
+    user.imageUrl = createUserDto.imageUrl;
+
+    return await user.save();
+  }
+
+  async findUserByUsername(username: string): Promise<User> {
+    return await User.findOne<User>({ where: { username } });
+  }
+
+  async editUser(createUserDto: CreateUserDto, id: string): Promise<User> {
+    return await User.update<User>(createUserDto, { where: { id } });
+  }
+
+  // would want to inject houseService and have func that finds by key?
+  async joinHouse(key: string, id: string): Promise<House> {
+    const house = await House.findOne<House>({ where: { key } });
+    await User.update({ houseId: house.id }, { where: { id } });
+    return house;
+  }
+}
